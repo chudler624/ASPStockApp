4a8df8a4c140c5317beb3e8aef11cc3798268fa7
Casey Hudler <chudler624@gmail.com>
2022-09-28 13:12:47 -0400

    api page created

---
+++ Stock/Models/StockApi.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Core;
+
+namespace Stock.Models
+{
+    public class StockApi
+    {
+
+        var client = new RestClient("https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact");
+        var request = new RestRequest(Method.GET);
+        Request.AddHeader("X-RapidAPI-Key", "fe96c29976msh7164fa7f072bef1p19efc6jsn0ad83cbe2bb2");
+        request.AddHeader("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com");
+        IRestResponse response = client.Execute(request);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
